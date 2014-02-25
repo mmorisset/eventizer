94b86f2fee734cfec88be354c288a61dcc11d58f
Maxime Morisset <maxime.morisset@t-immersion.com>
2014-02-25 20:39:57 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventizer::Application.config.secret_token = 'e1450ded7f55d5ba7921c011cbee8c2806999586a9a997889cdaaea9dd439393107c73f5db78c4f05357690dece51b87a58fb4c898ebae8c28a054975bed82e6'
